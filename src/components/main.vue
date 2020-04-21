0005f4a9cd536bc56adf3642d583ec6f1241e3f5
zhazhazsy <1006942332@qq.com>
2020-04-21 22:50:14 +0800

    首页轮播

---
+++ src/components/main.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bodysty">
+	  <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+	    <van-swipe-item class="lun1"></van-swipe-item>
+	    <van-swipe-item class="lun2">2</van-swipe-item>
+	    <van-swipe-item class="lun3">3</van-swipe-item>
+	    <van-swipe-item class="lun4">4</van-swipe-item>
+	  </van-swipe>
+  </div>
+</template>
+<style>
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+  .bodysty{
+	  height: 100%;
+  }
+  .my-swipe{
+	  position: absolute;left: 0;top: 50;
+	  width: 100%;
+	  height: auto;
+  }
+  .lun2{
+  	  width: 100%;
+  	  height: auto;
+  	  background-image: url(../assets/lunbo/img002.png);
+  	  background-size: 100%;
+  	  background-position: 0;
+  }
+  .lun3{
+  	  width: 100%;
+  	  height: auto;
+  	  background-image: url(../assets/lunbo/img003.png);
+  	  background-size: 100%;
+  	  background-position: 0;
+  }
+  .lun4{
+  	  width: 100%;
+  	  height: auto;
+  	  background-image: url(../assets/lunbo/img004.png);
+  	  background-size: 100%;
+  	  background-position: 0;
+  }
+  .lun1{
+  	  width: 100%;
+  	  height: auto;
+  	  background-image: url(../assets/lunbo/img001.png);
+  	  background-size: 100%;
+  	  background-position: 0;
+  }
+</style>
+
+
+
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button } from 'vant';
+import { Swipe, SwipeItem } from 'vant';
+
+
+
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(Button);
+
 
 Vue.config.productionTip = false;
 
